a887ae96f0dd61f42061113e2ea93a65090dea78
Meep <henrylurssen@gmail.com>
2022-05-04 18:36:58 -0400

    first-jsx

---
+++ first-jsx/src/App.js
@@ -0,0 +1,29 @@
+// import logo from './logo.svg';
+// import './App.css';
+
+function App() {
+  return (
+    <div className = "App">
+      <h1>Some of My Favorite Animes (Not including the Big 3)</h1>
+      <ul>
+        <li> Tokyo Ghoul </li>
+        <li> Attack on Titan </li>
+        <li> K Project</li>
+        <li> Bungo Stray Dogs </li>
+        <li> Aharen-San wa Hakarenai </li>
+        <li> Haikyuu </li>
+        <li> Fire Force </li>
+        <li> Dress Up Darling </li>
+        <li> Jujutsu Kaisen </li>
+        <li> Gintama </li>
+        <li> Platinum End </li>
+        <li> Assassination Classroom </li>
+        <li> Konosuba </li>
+        <li> Classroom of the Elite </li>
+        <li> Orange </li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
